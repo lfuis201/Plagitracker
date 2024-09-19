b23c8982b278391fd12c0faac499e21f7e174d06
DarkFero23 <luizln760@gmail.com>
2024-09-18 23:04:00 -0500

    Scraping with url and json validation

--- Backend/PlagiTracker/PlagiTracker.Services/SeleniumServices/Web_Scraping.cs
+++ Backend/PlagiTracker/PlagiTracker.Services/SeleniumServices/Web_Scraping.cs
@@ -1,83 +1,134 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using System.Diagnostics;
+using PlagiTracker.Services.Reportes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PlagiTracker.Services.SeleniumServices
 {
     internal class Web_Scraping
     {
-        //Declaramos de manera privada el driver de Selenium
         private IWebDriver driver;
-        private int folderCounter = 1;
-
 
         public Web_Scraping()
         {
-            //Esta parte se tiene que modificar dependiendo de la ubicación del chromedriver y dependiendo de la versión de Chrome que tengas instalada (en mi caso es el 128) 
             var chromeDriverService = ChromeDriverService.CreateDefaultService(@"C:\Users\Luis\Downloads\Chrome Selenium\chromedriver-win64 (2)\chromedriver-win64");
             chromeDriverService.HideCommandPromptWindow = false;
             var options = new ChromeOptions();
-            //Todos estos argumentos son opcionales, aunque recomendables que los tengas 
 
             options.AddArgument("--disable-usb");
-            options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");  // User-Agent
+            options.AddArgument("--headless");
+
+            options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");
             options.AddExcludedArgument("enable-automation");
             options.AddAdditionalOption("useAutomationExtension", false);
-            //Aca se inicializa el WebDriver con el servicio y las opciones configuradas
+
             driver = new ChromeDriver(chromeDriverService, options);
         }
 
-        //Esta funcion toma como argumento una lista de URLS que estan mas abajo, y se encarga de hacer el scraping de cada una de ellas, ( a modo de prueba para ver el tiempo)
-        public void StartScraping(List<string> urls)
+        private async Task<bool> UrlExists(string url)
         {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool IsCodivaUrl(string url)
+        {
+            // Verifica si la URL contiene "codiva.io"
+            return url.Contains("codiva.io");
+        }
+
+        public async Task StartScraping(List<string> urls)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var jsonData = new Dictionary<string, List<Dictionary<string, string>>>();
+
             try
             {
                 foreach (var url in urls)
                 {
-                    //Aca creamos una carpeta nueva para cada URL que se le pase, y se le pone un timestamp para que sea unico y no se sobreescriba
-                    string baseDirectory = @"D:\WS Text\Codigo";
-                    string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-                    string newDirectory = Path.Combine(baseDirectory, $"Codigo_{timestamp}");
+                    if (!IsCodivaUrl(url))
+                    {
+                        Console.WriteLine($"La URL {url} no es de Codiva y se omitirá.");
+                        continue;
+                    }
 
-                    //Crea la carpeta si no existe 
-                    Directory.CreateDirectory(newDirectory);
+                    bool urlValida = await UrlExists(url);
 
-                    //Navega a la URL
+                    if (!urlValida)
+                    {
+                        Console.WriteLine($"La URL {url} no es válida. Se omitirá.");
+                        continue;
+                    }
+
+                    // Si la URL es válida y es de Codiva, proceder con el scraping
                     driver.Navigate().GoToUrl(url);
-                    //Espera 800 milisegundos para que cargue la página(esto es muy importante, traten de jugar con el tiempo, ya que sin esto no scrapea las urls)
-                    Thread.Sleep(800);
+                    Thread.Sleep(1400);
 
-                    // Encuentra todos los elementos <label> con el título de la clase
                     var labels = driver.FindElements(By.XPath("//label[starts-with(@for, 'tab-java-')]"));
+                    string studentId =Guid.NewGuid().ToString(); 
+
+                    var studentFiles = new List<Dictionary<string, string>>();
 
                     foreach (var label in labels)
                     {
-                        // Obtenemos el nombre de las clases
                         string className = label.GetAttribute("title");
-
-                        // Selecciona la clase por su nombre
                         var tabElement = driver.FindElement(By.XPath($"//label[@title='{className}']"));
                         tabElement.Click();
 
-                        // Extrae el código de la clase
-                        var codeElements = driver.FindElements(By.XPath("//div[contains(@class, 'CodeMirror-code')]//pre"));
+                        var codeElements = driver.FindElements(By.XPath("//div[contains(@class,'CodeMirror-code')]//pre"));
+                        string codeContent = string.Join("\n", codeElements.Select(e => e.Text));
 
-                        // Genera una ruta de archivo única dentro de la nueva carpeta
-                        string filePath = Path.Combine(newDirectory, $"{className}.txt");
+                        var fileData = new Dictionary<string, string>
+                {
+                    { "nombre", $"{className}.java" },
+                    { "contenido", codeContent }
+                };
 
-                        using (StreamWriter writer = new StreamWriter(filePath))
-                        {
-                            foreach (var codeElement in codeElements)
-                            {
-                                // Escribe el código en el archivo, línea por línea(hay un error que se tiene que solucionar, cuando se extrae el codigo algunas veces le pone como 20 lienas en blanco al inicio)
-                                writer.WriteLine(codeElement.Text);
-                            }
-                        }
-                        //Imprime un mensaje en la consola para indicar que el scraping ha terminado
-                        Console.WriteLine($"Scraping terminado para {className} y código guardado en el archivo: {filePath}");
+                        studentFiles.Add(fileData);
+                        Console.WriteLine($"Scraping terminado para {className}");
                     }
-                    //Imprime un mensaje en la consola para indicar que el scraping ha terminado
-                    Console.WriteLine($"Scraping completo para la URL: {url}");
+
+                    // Añadir los datos solo si se obtuvieron archivos de código
+                    if (studentFiles.Count > 0)
+                    {
+                        jsonData[studentId] = studentFiles;
+                        Console.WriteLine($"Scraping completo para la URL: {url}");
+                    }
                 }
+
+                if (jsonData.Count > 0)
+                {
+                    string jsonOutput = JsonConvert.SerializeObject(jsonData, Formatting.Indented);
+                    string jsonFilePath = @"D:\WS Text\Codigo\codigos.json";
+                    File.WriteAllText(jsonFilePath, jsonOutput);
+                    Console.WriteLine($"Datos guardados en formato JSON en: {jsonFilePath}");
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró código válido para las URLs proporcionadas.");
+                }
+
+                sw.Stop();
+                Console.WriteLine("Tiempo transcurrido: {0}", sw.Elapsed.ToString("hh\\:mm\\:ss\\.fff"));
             }
             catch (Exception ex)
             {
@@ -92,21 +143,35 @@ namespace PlagiTracker.Services.SeleniumServices
 
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            //ACA AÑAIDR MAS URLS PARA PROBAR EL TIEMPO , CON 6 URLS TARDA MENOS DE 15 SEGUNDOS"
+            //Falta solucionar que cuando encuentre una url que no es no genere su json vacio
             Web_Scraping scraper = new Web_Scraping();
             List<string> urls = new List<string>
             {
                 "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
-                "https://www.codiva.io/p/20816608-16ad-45ca-b812-9406105efbc3",
-                                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
-                "https://www.codiva.io/p/20816608-16ad-45ca-b812-9406105efbc3",
+                "https://www.invalid-url.com",  
+                "https://www.youtube.com/watch?v=TpNDSyDnUwc", 
+                "https://www.codiva.io/p/valid-url", 
                 "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
-                "https://www.codiva.io/p/20816608-16ad-45ca-b812-9406105efbc3"
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://classroom.google.com/c/NzA0MDM0NzM0MzYy",
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://www.codiva.io/p/dbc162b6-5afe-46bf-b4b3-ee42f11c37c3",
+                "https://chatgpt.com/c/66e8b455-d26c-8005-90e1-7fbb273e3801",
+                "https://classroom.google.com/c/NzA0MDM0NzM0MzYy",
+                "https://www.fundeu.es/recomendacion/colaboracion-posible-alternativa-a-featuring/",
 
             };
-            scraper.StartScraping(urls);
+
+            await scraper.StartScraping(urls);
+
+            ReporteScraping reporte = new ReporteScraping();
+          
         }
     }
 }
