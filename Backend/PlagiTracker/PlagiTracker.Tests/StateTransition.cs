cf20d9a38f3acb6aa8c04075fed5a0da8a57adca
DAOBLUR <102988549+DAOBLUR@users.noreply.github.com>
2024-09-29 20:11:32 -0500

    Backend -  Tests: Errors solved

---
+++ Backend/PlagiTracker/PlagiTracker.Tests/StateTransition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlagiTracker.Tests
+{
+    public class StateTransition
+    {
+        private int attemptCount = 0;
+        private const int MaxAttempts = 3;
+
+        public string LogIn(string password, string correctPassword)
+        {
+            if (password == correctPassword)
+            {
+                attemptCount = 0;
+                return "Access Accepted";
+            }
+            else
+            {
+                attemptCount++;
+                if (attemptCount >= MaxAttempts)
+                {
+                    return "Account Blocked";
+                }
+                else
+                {
+                    return "Incorrect Password";
+                }
+            }
+        }
+    }
+}
