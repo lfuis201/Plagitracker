f94d152e6b56d5525d6db4ebe7a7fedd94d28602
DAOBLUR <102988549+DAOBLUR@users.noreply.github.com>
2024-11-13 06:58:25 -0500

    DataBase: All Backup - Wednesday, November 13, 2024, 06:55:39 AM

---
+++ DataBase/_Assignments__202411061612.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."Assignments" ("Id","Description","Title","SubmissionDate","AnalysisDate","IsAnalyzed","CourseId","CreatedAt","IsEnabled","UpdatedAt") VALUES
+	 ('ac85b4e5-33ea-4d2c-a281-f7ec41da15f7','testing','Testing','2024-12-07 10:14:26.713-05','2024-10-09 18:40:04.711558-05',true,'5500125e-16f5-4b77-9220-cc4dab69c100','-infinity',false,'-infinity');
---
+++ DataBase/_Courses__202411061612.sql
@@ -0,0 +1,4 @@
+INSERT INTO public."Courses" ("Id","Name","TeacherId","CreatedAt","IsEnabled","UpdatedAt","IsArchived") VALUES
+	 ('9c4f589b-d6c4-4e7a-b1d2-ece3dc9ee4f6','Cálculo 2','0392d24c-a1f5-4022-8278-cfa4efb14b8c','-infinity',false,'-infinity',false),
+	 ('bf9ee4c7-b051-4dd8-ba56-101f2688888c','Gestion','0392d24c-a1f5-4022-8278-cfa4efb14b8c','-infinity',false,'-infinity',true),
+	 ('5500125e-16f5-4b77-9220-cc4dab69c100','Calculo 1','0392d24c-a1f5-4022-8278-cfa4efb14b8c','-infinity',false,'-infinity',true);
