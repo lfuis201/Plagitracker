992dc1357009713211c2bb3424776034f7fcb05a
DAOBLUR <102988549+DAOBLUR@users.noreply.github.com>
2024-12-11 09:36:59 -0500

    Backend: Release v3.2.0

--- Backend/Release/EFSQLScripts/PlagiTracker.Data.DataAccess.DataContext.sql
+++ Backend/Release/EFSQLScripts/PlagiTracker.Data.DataAccess.DataContext.sql
@@ -1077,3 +1077,36 @@ BEGIN
 END $EF$;
 COMMIT;
 
+START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241209142107_UpdateAssignmentTitleConstraint') THEN
+    DROP INDEX "IX_Assignments_CourseId";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241209142107_UpdateAssignmentTitleConstraint') THEN
+    DROP INDEX "IX_Assignments_Title";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241209142107_UpdateAssignmentTitleConstraint') THEN
+    CREATE UNIQUE INDEX "IX_Assignments_CourseId_Title" ON "Assignments" ("CourseId", "Title");
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241209142107_UpdateAssignmentTitleConstraint') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20241209142107_UpdateAssignmentTitleConstraint', '8.0.8');
+    END IF;
+END $EF$;
+COMMIT;
+
